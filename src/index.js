65c518c74382eec160a550eb19d295ac7eb28ff6
Hendry <hendry.sadrak@outlook.com>
2020-03-17 20:48:53 +0200

    modern exports

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
-import SettingsDividerShort from './divider/divider-short';
-import SettingsDividerLong from './divider/divider-long';
-import SettingsEditText from './edit-text/edit-text';
-import SettingsPicker from './picker/picker';
-import SettingsCategoryHeader from './category-header/category-header';
-import SettingsTextLabel from './text-label/text-label';
-import SettingsSwitch from './switch/switch';
-import SettingsButton from './button/button';
-
-export {
-  SettingsDividerShort,
-  SettingsDividerLong,
-  SettingsEditText,
-  SettingsPicker,
-  SettingsCategoryHeader,
-  SettingsTextLabel,
-  SettingsSwitch,
-  SettingsButton,
-};
+export { default as SettingsDividerShort } from "./divider/divider-short";
+export { default as SettingsDividerLong } from "./divider/divider-long";
+export { default as SettingsEditText } from "./edit-text/edit-text";
+export { default as SettingsPicker } from "./picker/picker";
+export { default as SettingsCategoryHeader } from "./category-header/category-header";
+export { default as SettingsTextLabel } from "./text-label/text-label";
+export { default as SettingsSwitch } from "./switch/switch";
+export { default as SettingsButton } from "./button/button";
